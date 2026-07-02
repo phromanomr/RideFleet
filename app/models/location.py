299001e3c0e30fb06480d40c9587756cc6741f70
Phelipe Romano <35852613+phromanomr@users.noreply.github.com>
2026-07-01 23:35:41 -0300

    fix: correção location

--- app/models/location.py
+++ app/models/location.py
@@ -3,10 +3,10 @@ from pydantic import BaseModel, Field
 class Location(BaseModel):
     lat: float
     lng: float
-    street: str
-    number: str
-    city: str
-    state: str
+    street: str | None = None
+    number: str | None = None
+    city: str | None = None
+    state: str | None = None
 
 class GeoLocation(BaseModel):
     lat: float
